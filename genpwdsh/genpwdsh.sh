3f3b65bc3478f0154bb44967b2a3288142a88915
Ivan Kmeťo <32756443+kkmzero@users.noreply.github.com>
2021-09-02 10:36:14 +0200

    Update genpwdsh.sh

--- genpwdsh/genpwdsh.sh
+++ genpwdsh/genpwdsh.sh
@@ -6,17 +6,36 @@
 # CC0 1.0 Universal (CC0 1.0) Public Domain Dedication
 # https://creativecommons.org/publicdomain/zero/1.0/
 
-# Use: genpwdsh [length] [method]
-# Methods: -sha224 | -sha256 | -sha384 | -sha512 | -b2 | -md5 | -random  | -urandom | -openssl | -gpg
+# Usage: genpwdsh [LENGTH] [METHOD]
+# Methods: -sha224 | -sha256 | -sha384 | -sha512 | -b2 | -md5 | -random | -urandom | -openssl | -gpg
 
+
+if [ $# = 0 ]; then
+  echo 1>&2 "$0: Not enough arguments."
+  echo "Try 'genpwdsh --help' for more information."
+  exit 2
+fi
+
+if [ $1 = --help ]; then
+  echo "Usage: genpwdsh [LENGTH] [METHOD]"
+  echo "Bash script for generating passwords.\n"
+  echo "Available methods for generating passwords:"
+  echo "-sha224 | -sha256 | -sha384 | -sha512 | -b2 | -md5 | -random | -urandom | -openssl | -gpg"
+  return
+elif [ $1 = --version ]; then
+  echo "GenPwdSh 1.0.0"
+  echo "2020 - 2021, Ivan Kmeťo"
+  echo "CC0 1.0 Universal (CC0 1.0) Public Domain Dedication"
+  return
+fi
 
 if [ $# -lt 2 ]; then
   echo 1>&2 "$0: Not enough arguments."
-  echo 1>&2 "$0: Use: genpwdsh [length] [method]"
+  echo "Try 'genpwdsh --help' for more information."
   exit 2
 elif [ $# -gt 2 ]; then
   echo 1>&2 "$0: Too many arguments."
-  echo 1>&2 "$0: Use: genpwdsh [length] [method]"
+  echo "Try 'genpwdsh --help' for more information."
   exit 2
 fi
 
@@ -59,7 +78,7 @@ else
     gpg --gen-random --armor 2 $length | head -c $length; echo;
   else
     echo 1>&2 "$0: Unknown method."
-    echo 1>&2 "$0: Use: genpwdsh [length] [method]"
+    echo "Try 'genpwdsh --help' for more information."
     exit 2
   fi
 fi
