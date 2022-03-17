057e643ec30229e7fb84d41c879eff90476191ef
Ivan Kmeťo <32756443+kkmzero@users.noreply.github.com>
2022-03-17 23:18:12 +0100

    Update genpwdsh.sh

--- genpwdsh/genpwdsh.sh
+++ genpwdsh/genpwdsh.sh
@@ -7,7 +7,7 @@
 # https://creativecommons.org/publicdomain/zero/1.0/
 
 # Usage: genpwdsh [LENGTH] [METHOD] [INPUT]
-# Methods: -sha224 | -sha256 | -sha384 | -sha512 | -b2 | -random | -urandom | -openssl | -gpg
+# Methods: -sha224 | -sha256 | -sha384 | -sha512 | -b2 | -shake128 | -shake256 | -random | -urandom | -openssl | -gpg
 
 
 length=$1
@@ -16,8 +16,8 @@ input=$3
 
 
 if [ $# = 0 ]; then
-  length=8
-  method=-b2
+  length=16
+  method=-urandom
   input="date +%N%M%S"
 elif [ $# = 1 ]; then
   if [ $1 = --help ]; then
@@ -25,7 +25,7 @@ elif [ $# = 1 ]; then
     echo "Bash script for generating passwords."
     echo
     echo "Available methods for generating passwords:"
-    echo "-sha224 | -sha256 | -sha384 | -sha512 | -b2 | -random | -urandom | -openssl | -gpg"
+    echo "-sha224 | -sha256 | -sha384 | -sha512 | -b2 | -shake128 | -shake256 | -random | -urandom | -openssl | -gpg"
     exit 0
   elif [ $1 = --version ]; then
     echo "GenPwdSh 1.2.0"
@@ -33,7 +33,7 @@ elif [ $# = 1 ]; then
     echo "CC0 1.0 Universal (CC0 1.0) Public Domain Dedication"
     exit 0
   else
-    method=-b2
+    method=-urandom
     input="date +%N%M%S"
   fi
 elif [ $# = 2 ]; then
@@ -48,25 +48,31 @@ then
 else
   if [ $method = -sha224 ]
   then
-    $input | sha224sum | base64 | head -c $length; echo
+    $input | openssl dgst -sha3-224 -r | base64 | head -c $length; echo
   elif [ $method = -sha256 ]
   then
-    $input | sha256sum | base64 | head -c $length; echo
+    $input | openssl dgst -sha3-256 -r | base64 | head -c $length; echo
   elif [ $method = -sha384 ]
   then
-    $input | sha384sum | base64 | head -c $length; echo
+    $input | openssl dgst -sha3-384 -r | base64 | head -c $length; echo
   elif [ $method = -sha512 ]
   then
-    $input | sha512sum | base64 | head -c $length; echo
+    $input | openssl dgst -sha3-512 -r | base64 | head -c $length; echo
   elif [ $method = -b2 ]
   then
-    $input | b2sum | base64 | head -c $length; echo
+    $input | openssl dgst -blake2b512 -r | base64 | head -c $length; echo
+  elif [ $method = -shake128 ]
+  then
+    $input | openssl dgst -shake128 -r | base64 | head -c $length; echo
+  elif [ $method = -shake256 ]
+  then
+    $input | openssl dgst -shake256 -r | base64 | head -c $length; echo
   elif [ $method = -random ]
   then
-    < /dev/random tr -dc _A-Z-a-z-0-9 | head -c${1:-$length}; echo;
+    < /dev/random tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' | head -c${1:-$length}; echo;
   elif [ $method = -urandom ]
   then
-    < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-$length}; echo;
+    < /dev/urandom tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' | head -c${1:-$length}; echo;
   elif [ $method = -openssl ]
   then
     openssl rand -base64 $length | head -c $length; echo;
